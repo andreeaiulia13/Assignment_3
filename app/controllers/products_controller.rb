d56060cc7038b487bd23d8085cdb2f9fb83eb0b3
andreeaiulia13 <andreeagherman1320@gmail.com>
2021-05-05 10:35:38 +0300

    Create an error page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,21 +17,33 @@ class ProductsController < ApplicationController
   end
 
   def create
-    product = Product.create!(product_params.except(:image))
-    product.image.attach(product_params[:image])
-    redirect_to dashboard_path
+    begin
+      product = Product.create!(product_params.except(:image))
+      product.image.attach(product_params[:image])
+      
+      redirect_to dashboard_path
+    
+    rescue => exception
+      redirect_to new_error_product_path
+    end
   end
-
+  
   def edit
   end
 
+  def new_error
+    @product = Product.new
+  end
+
   def update
-   @product.update(product_params)
-   redirect_to dashboard_path
+    @product.update(product_params)
+   
+    redirect_to dashboard_path
   end
 
   def destroy
     @product.destroy
+    
     redirect_to dashboard_path
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :products
   get 'dashboard' => 'products#dashboard'
+  
+  get 'new_error_product' => 'products#new_error'
+  post 'new_error_product' => 'products#new_error'
+ 
   get 'products/index'
   root 'products#index'
 end
